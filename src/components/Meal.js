5f097be76942d80ceccd06f5bd46a94cbd5c3ca5
Jack Beeken <beekenj@gmail.com>
2023-11-21 10:48:14 -0700

    format MealList component

--- src/components/Meal.js
+++ src/components/Meal.js
@@ -2,9 +2,11 @@ import './Meal.css'
 
 export default function Meal(props) {
     return (
-      <div className='meal-elem'>
-        <div className='day-elem'>{props.day}</div>
-        <div className='name-elem'>{props.meal}</div>
+      <div className='meal-container'>
+        <div className='meal-elem'>
+          <div className='day-elem'>{props.day}</div>
+          <div className='name-elem'>{props.meal}</div>
+        </div>
       </div>
     )
   }
